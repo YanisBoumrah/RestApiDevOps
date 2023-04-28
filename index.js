0c7243d0985947960ee89e41141133e3479d736d
YanisBoumrah <yanis.boumrah@gmail.com>
2023-04-28 12:44:06 +0200

    adding put and delete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur l\'API REST !');
+  res.send('Bienvenue sur notre Api !');
 });
 
 app.get('/utilisateurs', (req, res) => {
@@ -18,10 +18,30 @@ app.get('/utilisateurs', (req, res) => {
   res.json(utilisateurs);
 });
 
+app.get('/utilisateurs/:id', (req, res) => {
+  const id = req.params.id;
+  // code pour récupérer l'utilisateur avec l'ID spécifié dans la base de données
+  const utilisateur = { id: id, nom: 'Jean', age: 30 };
+  res.json(utilisateur);
+});
+
 app.post('/utilisateurs', (req, res) => {
   const utilisateur = req.body;
   // code pour ajouter l'utilisateur à la base de données
   res.send('Utilisateur ajouté avec succès');
+});
+
+app.put('/utilisateurs/:id', (req, res) => {
+  const id = req.params.id;
+  const utilisateur = req.body;
+  // code pour mettre à jour l'utilisateur avec l'ID spécifié dans la base de données
+  res.send(`Utilisateur avec l'ID ${id} mis à jour avec succès`);
+});
+
+app.delete('/utilisateurs/:id', (req, res) => {
+  const id = req.params.id;
+  // code pour supprimer l'utilisateur avec l'ID spécifié de la base de données
+  res.send(`Utilisateur avec l'ID ${id} supprimé avec succès`);
 });
 
 app.listen(port, () => {
